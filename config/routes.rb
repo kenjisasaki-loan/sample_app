9becf773c2b2593419a8ee3ea53a1dbe09933888
kenjisasaki-loan <sasaki.kenji@home-loan.jp>
2024-08-14 17:33:09 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root "application#hello"
+  root "static_pages#home"
+  get  "static_pages/home"
+  get  "static_pages/help"
+  get  "static_pages/about"
 end
